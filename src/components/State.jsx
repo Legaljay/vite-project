6648fd4de98ce96522a8afa7582f9e603bb0caad
Legaljay <adesinaayobami67@gmail.com>
2023-10-05 22:32:51 +0100

    hi

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -11,7 +11,6 @@ export const State = () => {
     <div>
         <div className="text-4xl text-white mb-10">{name}</div>
         <button onClick={handleChange} className='bg-yellow-600 p-5 text-white rounded-xl'>Click Me</button>
-        /
     </div>
   )
 }
